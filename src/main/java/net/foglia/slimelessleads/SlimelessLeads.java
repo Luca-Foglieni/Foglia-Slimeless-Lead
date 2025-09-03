88d341c4e27a12cc30145f8bbe6391feb27233f5
Luca <luca.foglieni.work648@passfwd.com>
2025-09-03 20:09:23 +0200

    replaced the old recipe with the silmeball with a new one

--- src/main/java/net/foglia/slimelessleads/SlimelessLeads.java
+++ src/main/java/net/foglia/slimelessleads/SlimelessLeads.java
@@ -1,32 +1,27 @@
-package net.foglia.irontippedarrows;
+package net.foglia.slimelessleads;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(IronTippedArrows.MOD_ID)
-public class IronTippedArrows
+@Mod(SlimelessLeads.MOD_ID)
+public class SlimelessLeads
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "fogliairontippedarrows";
+    public static final String MOD_ID = "fogliaslimelessleads";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public IronTippedArrows()
+    public SlimelessLeads()
     {
-    }
+    }<
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
